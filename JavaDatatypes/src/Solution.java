50c4d8846c6e715550ff2fbab46d52cb7a3b8dcf
rusta <rustekapl@gmail.com>
2022-08-09 16:20:56 +0200

    Add JavaDatatypes

---
+++ JavaDatatypes/src/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = Integer.parseInt(scanner.nextLine());
+
+        String[] datas = new String[t];
+        for (int i = 0; i < t; i++) {
+            datas[i] = scanner.nextLine();
+
+        }
+    }
+
+
+}
