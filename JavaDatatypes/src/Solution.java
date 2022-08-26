0272d4ebc3dfec54bfe9b8b78479057e5aece5e1
rusta <rustekapl@gmail.com>
2022-08-26 14:46:37 +0200

    Add JavaDatatypes

--- JavaDatatypes/src/Solution.java
+++ JavaDatatypes/src/Solution.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Solution {
@@ -9,7 +10,34 @@ public class Solution {
         String[] datas = new String[t];
         for (int i = 0; i < t; i++) {
             datas[i] = scanner.nextLine();
+            showPremitive(datas[i]);
 
+        }
+    }
+
+    public static void showPremitive(Object input){
+        try{
+            if (input instanceof Short)
+                System.out.println("Primitive : short");
+            else if(input instanceof Integer)
+                System.out.println("Primitive : int");
+            else if(input instanceof Long)
+                System.out.println("Primitive : long");
+            else if(input instanceof Float)
+                System.out.println("Primitive : float");
+            else if(input instanceof Double)
+                System.out.println("Primitive : double");
+            else if(input instanceof Boolean)
+                System.out.println("Primitive : bool");
+            else if(input instanceof Character)
+                System.out.println("Primitive : char");
+            else if(input instanceof Byte)
+                System.out.println("Primitive : byte");
+            else
+                System.out.println("Reference : string");
+        }
+        catch (InputMismatchException e){
+            System.out.println("Exception occur = "+e);
         }
     }
 
---
+++ JavaStaticInitializerBlock/src/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+         int b = Integer.parseInt(scanner.nextLine());
+
+        int h = Integer.parseInt(scanner.nextLine());
+
+
+        if (b > 0 & h > 0) {
+            System.out.println(b * h);
+        } else {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+
+
+    }
+}
