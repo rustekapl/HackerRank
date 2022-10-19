790b6d5569d499274ceb11b47904dbe6791b9e91
rusta <rustekapl@gmail.com>
2022-10-19 15:43:00 +0200

    JavaEndOfFile complete

---
+++ JavaEndOfFile/src/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int count = 1;
+        try(Scanner in = new Scanner(System.in))
+        {
+            while(in.hasNext())
+            {
+                System.out.println(count + " " + in.nextLine());
+                ++count;
+            }
+        }
+
+    }
+}
